5d2ce12eb889eb662e8049d9345b46e9ed814681
Shoumq <lae345@mail.ru>
2023-07-10 19:44:30 +0300

    API 1.2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
 {
     /**
      * @OA\Get(
-     *     path="/api/",
+     *     path="/api/swagger",
      *     summary="Listing products",
      *     tags={"Product"},
      *     @OA\Response(
@@ -57,7 +57,7 @@ class ProductController extends Controller
 
     /**
      * @OA\Get(
-     *     path="/api/?title={title}",
+     *     path="/api/?title={title}/swagger",
      *     summary="Listing a product by title",
      *     tags={"Product"},
      *     @OA\Parameter(
@@ -94,7 +94,7 @@ class ProductController extends Controller
 
     /**
      * @OA\Get(
-     *     path="/api/{id}",
+     *     path="/api/{id}/swagger",
      *     summary="Listing a product by id",
      *     tags={"Product"},
      *     @OA\Parameter(
@@ -140,7 +140,7 @@ class ProductController extends Controller
 
     /**
      * @OA\Post(
-     *     path="/api/create",
+     *     path="/api/create/swagger",
      *     summary="Сreating a product according to the required parameters 'title', 'description', 'price'",
      *     tags={"Product"},
      *     @OA\RequestBody(
@@ -196,7 +196,7 @@ class ProductController extends Controller
 
     /**
      * @OA\Patch(
-     *     path="/api/{id}/edit",
+     *     path="/api/{id}/edit/swagger",
      *     summary="Product update by parameters 'title', 'description', 'price'",
      *     tags={"Product"},
      *     @OA\Parameter(
@@ -281,7 +281,7 @@ class ProductController extends Controller
 
     /**
      * @OA\Delete(
-     *     path="/api/{id}/delete",
+     *     path="/api/{id}/delete/swagger",
      *     summary="Delete product by id",
      *     tags={"Product"},
      *     @OA\Parameter(
